a28bd6c9bf4f8088d63f5a7fbdcdbf61fb2d5694
kennja05 <jacobkenny05@gmail.com>
2020-01-15 16:54:49 -0500

    almost done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,24 +2,40 @@ class OrdersController < ApplicationController
   
   def show
     @order = Order.find(params[:id])
+    
   end 
   
   
   def new
-    @fruits = Fruit.all
+    farm = Farm.find(params[:farm_id])
+    @fruits = farm.fruits
     @order = Order.new
     
   end
 
   def create
-    @order = Order.create!(order_params)
+    
+    farm = Farm.find(params[:farm_id])
+    fruits = farm.fruits.select {|fruit| params[fruit.name].to_i > 0 }
+    fruits.each do |fruit|
+      order = Order.create!(fruit: fruit, farm_id: params[:farm_id], quantity: params[fruit.name].to_i, restaurant_id: 1)
+      byebug
+    end 
+
     #render :new
     
-    redirect_to new_order_path
+    redirect_to restaurants_path
   end 
 
   def edit
+    @order = Order.find(params[:id])
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to order_path(@order)
+  end 
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resources :orders, only: [:show, :edit, :update]
   resources :farms, only: [:show] do
     resources :orders, only: [:new, :create]
   end
-  resources :orders, only: [:show]
   resources :restaurants, only: [:index, :show] do 
     resources :orders, only: [:show]
   end 
